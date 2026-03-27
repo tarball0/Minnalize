c3bc3a1031b48a11ddda00686fa5f795c17e9ff2
Fahad-uz <fahadmanaff@gmail.com>
2026-03-28 02:33:54 +0530

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -3,6 +3,9 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('desktopAPI', {
   pickFile: () => ipcRenderer.invoke('dialog:pickFile'),
   runAnalysis: (filePath) => ipcRenderer.invoke('analysis:run', filePath),
+  getLastAutoScanResult: () => ipcRenderer.invoke('autoscan:getLastResult'),
+  getLastAutoScanError: () => ipcRenderer.invoke('autoscan:getLastError'),
+  getDownloadsPath: () => ipcRenderer.invoke('system:getDownloadsPath'),
 
   onAutoScanResult: (callback) => {
     const handler = (_event, payload) => callback(payload);
@@ -14,8 +17,5 @@ contextBridge.exposeInMainWorld('desktopAPI', {
     const handler = (_event, payload) => callback(payload);
     ipcRenderer.on('autoscan:error', handler);
     return () => ipcRenderer.removeListener('autoscan:error', handler);
-  },
-
-  getLastAutoScanResult: () => ipcRenderer.invoke('autoscan:getLastResult'),
-  getLastAutoScanError: () => ipcRenderer.invoke('autoscan:getLastError')
+  }
 });
